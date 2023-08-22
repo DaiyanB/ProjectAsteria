d6858cfe67a4b872ae15f4191465a271a1de2a56
DaiyanB <daiyan.bhj@gmail.com>
2023-08-22 15:41:30 +0100

    Created base code

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame 
+# import math
+
+# from objects import Planet
+from objects import planets
+from objects import WIDTH, HEIGHT
+
+pygame.init()
+ 
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+# WIN = pygame.display.set_mode((0, 0))
+
+pygame.display.set_caption("Mona 1.0")
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        WIN.fill((0, 0, 0))
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        for planet in planets:
+            planet.update_position(planets)
+            planet.draw(WIN)
+
+        pygame.display.update()
+        
+    pygame.quit()
+
+main()
